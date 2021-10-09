523d97883b8d7e07992a0b50807fd5db3ce1359f
Lucas Colas <51634013+LucasColas@users.noreply.github.com>
2021-10-09 16:40:19 +0200

    deprecated methods
    sendRawTransaction and signTransaction are deprecated

--- examples/3_send_transactions.py
+++ examples/3_send_transactions.py
@@ -17,8 +17,8 @@ tx = {
     'gasPrice': web3.toWei('50', 'gwei'),
 }
 
-signed_tx = web3.eth.account.signTransaction(tx, private_key)
+signed_tx = web3.eth.account.sign_transaction(tx, private_key)
 
-tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
 
 print(web3.toHex(tx_hash))
